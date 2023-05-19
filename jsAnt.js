30e5d51f0e3031c1541733f8c2eea131eb4f530e
Reeza Maharjan <reezamhr.50@gamil.com>
2023-05-19 08:45:06 +0545

    updating

--- jsAnt.js
+++ jsAnt.js
@@ -6,30 +6,31 @@ class BallGenerate {
     
     //the funtions calling other functions to display ant and for collision
     init() {
+      this.storeBalls();
       this.createBalls();
-      this.startGame();
       this.collision();
     }
     
   
     //Creating ball object
-    createBall() {
+    storeBall() {
       let ball = {};
-      ball.x = Math.floor(Math.random() * (this.mainContainer.offsetWidth - 50 + 1));
-      ball.y = Math.floor(Math.random() * (this.mainContainer.offsetHeight- 50 +1));
+      ball.x = Math.floor(Math.random() * (this.mainContainer.offsetWidth - 40 + 1));
+      ball.y = Math.floor(Math.random() * (this.mainContainer.offsetHeight- 40 +1));
       ball.dx = Math.random();
       ball.dy = Math.random();
       return ball;
     }
     //Creating balls array containing ball object
-    createBalls() {
-      for (let i = 0; i < 20; i++) {
-        const ball = this.createBall();
+    storeBalls() {
+      var numAnt =20;
+      for (let i = 0; i < numAnt; i++) {
+        const ball = this.storeBall();
         this.balls.push(ball);
       }
     }
-    //Starts the game
-    startGame() {
+    //Display ants in the screen
+    createBalls() {
         for (const ball of this.balls) {
           this.drawBall(ball.x, ball.y);
           setInterval(() => {
@@ -42,10 +43,37 @@ class BallGenerate {
       const ball_el = document.createElement("div");//Creating a div for balls
       ball_el.classList.add("ball");
       ball_el.style.left = x + "px";
-      ball_el.style.bottom = y + "px";    
+      ball_el.style.bottom = y + "px"; 
+
+      ball_el.addEventListener("click", () => this.killAnt(ball));
+      // ball_el.addEventListener("click", killAnt(ball));
+
+      // ball_el.addEventListener("click", console.log(1));
+
       this.mainContainer.appendChild(ball_el);
-      
     }
+    
+    killAnt(){
+      const ball_el = document.querySelector(".ball");
+      if (ball_el){
+        this.mainContainer.removeChild(ball_el);
+        const ballIndex = this.balls.indexOf(ball);
+        if (ballIndex > - 1){
+          this.balls.splice(ballIndex, 1);
+          antsCount++;
+          scoreBoard();
+        }
+      }
+    }
+
+
+      scoreBoard() {
+        const scoreUpdate = document.getElementById("scoreBox");
+        scoreUpdate.innerHTML = `Score : ${antsCount}`;
+      }
+
+
+    
     //funtion to move the ants
     moveBall(ball) {
         ball.x = ball.x + ball.dx;
@@ -77,7 +105,7 @@ class BallGenerate {
           const dy = ball2.y - ball1.y;
           const distance = Math.sqrt(dx * dx + dy * dy);
         
-          if (distance < 19) { 
+          if (distance < 40) { 
             // Reverse the direction of both ants after collision
             ball1.dx *= -1;
             ball1.dy *= -1;
@@ -87,11 +115,25 @@ class BallGenerate {
         }
       }
     }
+
+    
+    
   }
-  
-  
+     
   const show = new BallGenerate();
   show.init();
+
+
+
+//   show.onclick="killAnt()";
+
+// killAnt(){
+//   var clickCount = 0;
+//   if (){
+//     clickCount += 1;
+//   }
+// }
+
 
 
 
